45e139ff908d3521d2c98ec7d8c3419897a05d7c
adehrt7 <petronela-adelina.hritcu@student.tuiasi.ro>
2026-04-09 12:54:51 +0300

    feat: update last commit for aligning with spotless

--- crypto-rc6/src/test/java/ro/p2p/crypto/core/RC6CipherTest.java
+++ crypto-rc6/src/test/java/ro/p2p/crypto/core/RC6CipherTest.java
@@ -11,11 +11,10 @@ import org.junit.jupiter.api.Test;
 
 class RC6CipherTest {
 
-    private static final byte[] KEY_128 =
-        hex("01 23 45 67 89 AB CD EF 01 23 45 67 89 AB CD EF");
+    private static final byte[] KEY_128 = hex("01 23 45 67 89 AB CD EF 01 23 45 67 89 AB CD EF");
 
     private static final byte[] PLAINTEXT_BLOCK =
-        hex("02 13 24 35 46 57 68 79 8A 9B AC BD CE DF E0 F1");
+            hex("02 13 24 35 46 57 68 79 8A 9B AC BD CE DF E0 F1");
 
     @Test
     void encryptThenDecryptReturnsOriginalPlaintext() {
@@ -93,53 +92,53 @@ class RC6CipherTest {
     @Test
     void officialVector128AllZeroKeyAndPlaintextMatchesExpectedCiphertext() {
         assertOfficialVector(
-            hex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),
-            hex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),
-            hex("8F C3 A5 36 56 B1 F7 78 C1 29 DF 4E 98 48 A4 1E"));
+                hex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),
+                hex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),
+                hex("8F C3 A5 36 56 B1 F7 78 C1 29 DF 4E 98 48 A4 1E"));
     }
 
     @Test
     void officialVector128CustomKeyAndPlaintextMatchesExpectedCiphertext() {
         assertOfficialVector(
-            hex("01 23 45 67 89 AB CD EF 01 12 23 34 45 56 67 78"),
-            hex("02 13 24 35 46 57 68 79 8A 9B AC BD CE DF E0 F1"),
-            hex("52 4E 19 2F 47 15 C6 23 1F 51 F6 36 7E A4 3F 18"));
+                hex("01 23 45 67 89 AB CD EF 01 12 23 34 45 56 67 78"),
+                hex("02 13 24 35 46 57 68 79 8A 9B AC BD CE DF E0 F1"),
+                hex("52 4E 19 2F 47 15 C6 23 1F 51 F6 36 7E A4 3F 18"));
     }
 
     @Test
     void officialVector192AllZeroKeyAndPlaintextMatchesExpectedCiphertext() {
         assertOfficialVector(
-            hex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 "
-                + "00 00 00 00 00 00 00 00"),
-            hex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),
-            hex("6C D6 1B CB 19 0B 30 38 4E 8A 3F 16 86 90 AE 82"));
+                hex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 " + "00 00 00 00 00 00 00 00"),
+                hex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),
+                hex("6C D6 1B CB 19 0B 30 38 4E 8A 3F 16 86 90 AE 82"));
     }
 
     @Test
     void officialVector192CustomKeyAndPlaintextMatchesExpectedCiphertext() {
         assertOfficialVector(
-            hex("01 23 45 67 89 AB CD EF 01 12 23 34 45 56 67 78 "
-                + "89 9A AB BC CD DE EF F0"),
-            hex("02 13 24 35 46 57 68 79 8A 9B AC BD CE DF E0 F1"),
-            hex("68 83 29 D0 19 E5 05 04 1E 52 E9 2A F9 52 91 D4"));
+                hex("01 23 45 67 89 AB CD EF 01 12 23 34 45 56 67 78 " + "89 9A AB BC CD DE EF F0"),
+                hex("02 13 24 35 46 57 68 79 8A 9B AC BD CE DF E0 F1"),
+                hex("68 83 29 D0 19 E5 05 04 1E 52 E9 2A F9 52 91 D4"));
     }
 
     @Test
     void officialVector256AllZeroKeyAndPlaintextMatchesExpectedCiphertext() {
         assertOfficialVector(
-            hex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 "
-                + "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),
-            hex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),
-            hex("8F 5F BD 05 10 D1 5F A8 93 FA 3F DA 6E 85 7E C2"));
+                hex(
+                        "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 "
+                                + "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),
+                hex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),
+                hex("8F 5F BD 05 10 D1 5F A8 93 FA 3F DA 6E 85 7E C2"));
     }
 
     @Test
     void officialVector256CustomKeyAndPlaintextMatchesExpectedCiphertext() {
         assertOfficialVector(
-            hex("01 23 45 67 89 AB CD EF 01 12 23 34 45 56 67 78 "
-                + "89 9A AB BC CD DE EF F0 10 32 54 76 98 BA DC FE"),
-            hex("02 13 24 35 46 57 68 79 8A 9B AC BD CE DF E0 F1"),
-            hex("C8 24 18 16 F0 D7 E4 89 20 AD 16 A1 67 4E 5D 48"));
+                hex(
+                        "01 23 45 67 89 AB CD EF 01 12 23 34 45 56 67 78 "
+                                + "89 9A AB BC CD DE EF F0 10 32 54 76 98 BA DC FE"),
+                hex("02 13 24 35 46 57 68 79 8A 9B AC BD CE DF E0 F1"),
+                hex("C8 24 18 16 F0 D7 E4 89 20 AD 16 A1 67 4E 5D 48"));
     }
 
     @Test
@@ -174,7 +173,8 @@ class RC6CipherTest {
         assertNotSame(subkeys1, subkeys2);
     }
 
-    private static void assertOfficialVector(byte[] key, byte[] plaintext, byte[] expectedCiphertext) {
+    private static void assertOfficialVector(
+            byte[] key, byte[] plaintext, byte[] expectedCiphertext) {
         RC6Cipher cipher = new RC6Cipher(key);
 
         byte[] ciphertext = cipher.encryptBlock(plaintext);
@@ -187,7 +187,8 @@ class RC6CipherTest {
     private static byte[] hex(String value) {
         String normalized = value.replaceAll("\\s+", "");
         if ((normalized.length() & 1) != 0) {
-            throw new IllegalArgumentException("Hex string must contain an even number of characters");
+            throw new IllegalArgumentException(
+                    "Hex string must contain an even number of characters");
         }
 
         byte[] bytes = new byte[normalized.length() / 2];
