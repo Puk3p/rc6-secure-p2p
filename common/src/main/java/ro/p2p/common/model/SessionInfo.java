55d5e7fc615c5d57f224785f510ed15e8cd67432
adehrt7 <petronela-adelina.hritcu@student.tuiasi.ro>
2026-04-28 16:27:08 +0300

    feat(crypto): implement Diffie-Hellman key agreement

--- common/src/main/java/ro/p2p/common/model/SessionInfo.java
+++ common/src/main/java/ro/p2p/common/model/SessionInfo.java
@@ -1,1 +1,29 @@
+package ro.p2p.common.model;
 
+import java.time.Instant;
+import java.util.Arrays;
+
+public class SessionInfo {
+
+    private final String peerId;
+    private final byte[] sessionKey;
+    private final Instant establishedAt;
+
+    public SessionInfo(String peerId, byte[] sessionKey, Instant establishedAt) {
+        this.peerId = peerId;
+        this.sessionKey = Arrays.copyOf(sessionKey, sessionKey.length);
+        this.establishedAt = establishedAt;
+    }
+
+    public String getPeerId() {
+        return peerId;
+    }
+
+    public byte[] getSessionKey() {
+        return Arrays.copyOf(sessionKey, sessionKey.length);
+    }
+
+    public Instant getEstablishedAt() {
+        return establishedAt;
+    }
+}
--- protocol/src/main/java/ro/p2p/protocol/packet/HelloPacket.java
+++ protocol/src/main/java/ro/p2p/protocol/packet/HelloPacket.java
@@ -1,1 +1,40 @@
+package ro.p2p.protocol.packet;
 
+import java.util.Arrays;
+
+public class HelloPacket {
+
+    private final String nodeId;
+    private final int listenPort;
+    private final byte[] publicKey;
+    private final byte[] nonce;
+    private final boolean response;
+
+    public HelloPacket(String nodeId, int listenPort, byte[] publicKey, byte[] nonce, boolean response) {
+        this.nodeId = nodeId;
+        this.listenPort = listenPort;
+        this.publicKey = Arrays.copyOf(publicKey, publicKey.length);
+        this.nonce = Arrays.copyOf(nonce, nonce.length);
+        this.response = response;
+    }
+
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    public int getListenPort() {
+        return listenPort;
+    }
+
+    public byte[] getPublicKey() {
+        return Arrays.copyOf(publicKey, publicKey.length);
+    }
+
+    public byte[] getNonce() {
+        return Arrays.copyOf(nonce, nonce.length);
+    }
+
+    public boolean isResponse() {
+        return response;
+    }
+}
